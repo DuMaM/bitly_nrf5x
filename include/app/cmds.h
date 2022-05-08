1496ae117499ba714a594c41a3a9da3b9dc86af9
Bartosz Nowak <9051964+DuMaM@users.noreply.github.com>
2022-05-08 13:36:41 +0200

    Split commands

--- include/app/cmds.h
+++ include/app/cmds.h
@@ -34,5 +34,7 @@ typedef struct
 
 void instruction_print(void);
 int default_cmd(const struct shell *shell, size_t argc, char **argv);
+const char *phy_str(const struct bt_conn_le_phy_param *phy);
+int8_t atob(const char *buffer);
 
 #endif /* _CMDS_H_ */
--- src/app/cmds.c
+++ src/app/cmds.c
@@ -7,14 +7,6 @@
 #include <cmds.h>
 #include <bt_test.h>
 
-test_params_t test_params = {
-    .conn_param = BT_LE_CONN_PARAM(INTERVAL_MIN, INTERVAL_MAX, CONN_LATENCY,
-                                   SUPERVISION_TIMEOUT),
-    .phy = BT_CONN_LE_PHY_PARAM_2M,
-    .data_len = BT_LE_DATA_LEN_PARAM_MAX,
-    .enable_rssi = true,
-};
-
 int default_cmd(const struct shell *shell, size_t argc, char **argv)
 {
     if (argc == 1)
@@ -38,7 +30,7 @@ int default_cmd(const struct shell *shell, size_t argc, char **argv)
     return 0;
 }
 
-static const char *phy_str(const struct bt_conn_le_phy_param *phy)
+const char *phy_str(const struct bt_conn_le_phy_param *phy)
 {
     static const char *const str[] = {
         "1 Mbps",
@@ -70,139 +62,6 @@ static const char *phy_str(const struct bt_conn_le_phy_param *phy)
     }
 }
 
-static int cmd_phy_1m(const struct shell *shell, size_t argc, char **argv)
-{
-    test_params.phy->options = BT_CONN_LE_PHY_OPT_NONE;
-    test_params.phy->pref_rx_phy = BT_GAP_LE_PHY_1M;
-    test_params.phy->pref_tx_phy = BT_GAP_LE_PHY_1M;
-
-    shell_print(shell, "PHY set to: %s", phy_str(test_params.phy));
-
-    return 0;
-}
-
-static int cmd_phy_2m(const struct shell *shell, size_t argc, char **argv)
-{
-    test_params.phy->options = BT_CONN_LE_PHY_OPT_NONE;
-    test_params.phy->pref_rx_phy = BT_GAP_LE_PHY_2M;
-    test_params.phy->pref_tx_phy = BT_GAP_LE_PHY_2M;
-
-    shell_print(shell, "PHY set to: %s", phy_str(test_params.phy));
-
-    return 0;
-}
-
-#if defined(RADIO_MODE_MODE_Ble_LR500Kbit) || defined(NRF5340_XXAA_APPLICATION)
-static int cmd_phy_coded_s2(const struct shell *shell, size_t argc, char **argv)
-{
-    test_params.phy->options = BT_CONN_LE_PHY_OPT_CODED_S2;
-    test_params.phy->pref_rx_phy = BT_GAP_LE_PHY_CODED;
-    test_params.phy->pref_tx_phy = BT_GAP_LE_PHY_CODED;
-
-    shell_print(shell, "PHY set to: %s", phy_str(test_params.phy));
-
-    return 0;
-}
-#endif /*                                            \
-        * defined(RADIO_MODE_MODE_Ble_LR1500Kbit) || \
-        * defined(NRF5340_XXAA_APPLICATION)          \
-        */
-
-#if defined(RADIO_MODE_MODE_Ble_LR125Kbit) || defined(NRF5340_XXAA_APPLICATION)
-static int cmd_phy_coded_s8(const struct shell *shell, size_t argc, char **argv)
-{
-    test_params.phy->options = BT_CONN_LE_PHY_OPT_CODED_S8;
-    test_params.phy->pref_rx_phy = BT_GAP_LE_PHY_CODED;
-    test_params.phy->pref_tx_phy = BT_GAP_LE_PHY_CODED;
-
-    shell_print(shell, "PHY set to: %s",
-                phy_str(test_params.phy));
-
-    return 0;
-}
-#endif /*                                           \
-        * defined(RADIO_MODE_MODE_Ble_LR125Kbit) || \
-        * defined(NRF5340_XXAA_APPLICATION)         \
-        */
-
-// data len
-static int data_len_cmd(const struct shell *shell, size_t argc, char **argv)
-{
-    uint16_t data_len;
-
-    if (argc == 1)
-    {
-        shell_help(shell);
-        return SHELL_CMD_HELP_PRINTED;
-    }
-
-    if (argc > 2)
-    {
-        shell_error(shell, "%s: bad parameters count", argv[0]);
-        return -EINVAL;
-    }
-
-    data_len = strtol(argv[1], NULL, 10);
-
-    if ((data_len < BT_GAP_DATA_LEN_DEFAULT) ||
-        (data_len > BT_GAP_DATA_LEN_MAX))
-    {
-        shell_error(shell, "%s: Invalid setting: %d", argv[0],
-                    data_len);
-        shell_error(shell,
-                    "LE Data Packet Length must be between: %d and %d",
-                    BT_GAP_DATA_LEN_DEFAULT, BT_GAP_DATA_LEN_MAX);
-        return -EINVAL;
-    }
-
-    test_params.data_len->tx_max_len = data_len;
-    test_params.data_len->tx_max_time = BT_GAP_DATA_TIME_MAX;
-
-    shell_print(shell, "LE Data Packet Length set to: %d", data_len);
-
-    return 0;
-}
-
-static int conn_interval_cmd(const struct shell *shell, size_t argc, char **argv)
-{
-    uint16_t interval;
-
-    if (argc == 1)
-    {
-        shell_help(shell);
-        return SHELL_CMD_HELP_PRINTED;
-    }
-
-    if (argc > 2)
-    {
-        shell_error(shell, "%s: bad parameters count", argv[0]);
-        return -EINVAL;
-    }
-
-    interval = strtol(argv[1], NULL, 10);
-
-    if ((interval < MIN_CONN_INTERVAL) ||
-        (interval > MAX_CONN_INTERVAL))
-    {
-        shell_error(shell, "%s: Invalid setting: %d", argv[0],
-                    interval);
-        shell_error(shell,
-                    "Connection interval must be between: %d and %d",
-                    MIN_CONN_INTERVAL, MAX_CONN_INTERVAL);
-        return -EINVAL;
-    }
-
-    test_params.conn_param->interval_max = interval;
-    test_params.conn_param->interval_min = interval;
-    test_params.conn_param->latency = 0;
-    test_params.conn_param->timeout = SUPERVISION_TIMEOUT;
-
-    shell_print(shell, "Connection interval set to: %d",
-                interval);
-
-    return 0;
-}
-
 int8_t atob(const char *buffer)
 {
 #define lookup_size 4
@@ -220,95 +79,3 @@ int8_t atob(const char *buffer)
 
     return -1;
 }
-
-static int rssi_cmd(const struct shell *shell, size_t argc, char **argv)
-{
-    if (argc == 1)
-    {
-        shell_help(shell);
-        return SHELL_CMD_HELP_PRINTED;
-    }
-
-    if (argc > 2)
-    {
-        shell_error(shell, "%s: bad parameters count", argv[0]);
-        return -EINVAL;
-    }
-
-    int8_t rssi = atob(argv[1]);
-    if (rssi < 0)
-    {
-        shell_error(shell, "Invalid parameter try true/y/1 or false/n/0");
-        return -EINVAL;
-    }
-
-    test_params.enable_rssi = (bool)rssi;
-    shell_print(shell, "RSSI measurement is set to: %s", rssi > 0 ? "true" : "false");
-
-    return 0;
-}
-
-static int print_cmd(const struct shell *shell, size_t argc,
-                     char **argv)
-{
-    shell_print(shell, "==== Current test configuration ====\n");
-    shell_print(shell, "Data length:\t\t%d\n"
-                       "Connection interval:\t%d units\n"
-                       "Preferred PHY:\t\t%s\n",
-                test_params.data_len->tx_max_len,
-                test_params.conn_param->interval_min,
-                phy_str(test_params.phy));
-    return 0;
-}
-
-void instruction_print(void)
-{
-    printk("\nType 'config' to change the configuration parameters.\n");
-    printk("You can use the Tab key to autocomplete your input.\n");
-    printk("Type 'run' when you are ready to run the test.\n");
-}
-
-SHELL_STATIC_SUBCMD_SET_CREATE(phy_sub,
-                               SHELL_CMD(1M, NULL, "Set preferred PHY to 1Mbps", cmd_phy_1m),
-                               SHELL_CMD(2M, NULL, "Set preferred PHY to 2Mbps", cmd_phy_2m),
-#if defined(RADIO_MODE_MODE_Ble_LR500Kbit) || defined(NRF5340_XXAA_APPLICATION)
-                               SHELL_CMD(coded_s2, NULL, "Set preferred PHY to Coded S2", cmd_phy_coded_s2),
-#endif
-#if defined(RADIO_MODE_MODE_Ble_LR125Kbit) || defined(NRF5340_XXAA_APPLICATION)
-                               SHELL_CMD(coded_s8, NULL, "Set preferred PHY to Coded S8", cmd_phy_coded_s8),
-#endif
-                               SHELL_SUBCMD_SET_END);
-
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_config,
-                               SHELL_CMD(data_length, NULL, "Configure data length", data_len_cmd),
-                               SHELL_CMD(conn_interval, NULL, "Configure connection interval <1.25ms units>", conn_interval_cmd),
-                               SHELL_CMD(phy, &phy_sub, "Configure connection interval", default_cmd),
-                               SHELL_CMD(rssi, NULL, "RSSI configuration y/n", rssi_cmd),
-                               SHELL_CMD(print, NULL, "Print current configuration", print_cmd),
-                               SHELL_SUBCMD_SET_END);
-
-SHELL_CMD_REGISTER(config, &sub_config, "Configure testing parameters", default_cmd);
-
-// add connect command
-static int set_role_to_slave(const struct shell *shell, size_t argc, char **argv)
-{
-    shell_print(shell, "\nSlave role. Starting advertising\n");
-    adv_start();
-
-    return 0;
-}
-
-static int set_role_to_master(const struct shell *shell, size_t argc, char **argv)
-{
-    shell_print(shell, "\nMaster role. Starting scanning\n");
-    scan_start();
-
-    return 0;
-}
-
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_role,
-                               SHELL_CMD(master, NULL, "Set master role - start listening for input", set_role_to_master),
-                               SHELL_CMD(slave, NULL, "Set slave role - start advertising", set_role_to_slave),
-                               SHELL_SUBCMD_SET_END);
-
-SHELL_CMD_REGISTER(connect, &sub_role, "Set device role", default_cmd);
