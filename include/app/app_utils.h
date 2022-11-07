cbec1f1f7af154299a0d23fa03ae7f743a3866a4
Bartosz Nowak <9051964+DuMaM@users.noreply.github.com>
2022-11-07 03:01:33 +0100

    fix: util file

--- include/app/app_utils.h
+++ include/app/app_utils.h
@@ -10,4 +10,9 @@ typedef struct dynamic_mean {
 
 void dm_update(dynamic_mean_t* dm, uint64_t new_value);
 
+uint32_t conv_raw_to_u24(uint8_t *raw, uint16_t pos);
+int32_t conv_u24_to_i32(uint32_t u24_val);
+uint32_t conv_i32_to_u24(int32_t i32_val);
+uint8_t *conv_u24_to_raw(uint32_t u24_val, uint8_t *raw, uint16_t pos);
+
 #endif // _MOVING_AVG_H_
--- src/app/app_utils.c
+++ src/app/app_utils.c
@@ -1,4 +1,4 @@
-#include <moving_avg.h>
+#include <app_utils.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,4 +12,30 @@ void dm_update(dynamic_mean_t *dm, uint64_t new_value)
     uint64_t differential = (new_value - dm->mean) / dm->count;
     uint64_t new_mean = dm->mean + differential;
     dm->mean = new_mean;
+}
+
+
+inline uint32_t conv_raw_to_u24(uint8_t *raw, uint16_t pos)
+{
+    return (((uint32_t)raw[pos + 0]) << 16) +
+           (((uint32_t)raw[pos + 1]) << 8) +
+           (((uint32_t)raw[pos + 2]) << 0);
+}
+
+inline int32_t conv_u24_to_i32(uint32_t u24_val)
+{
+    return ((int32_t)(u24_val << 8)) >> 8;
+}
+
+inline uint32_t conv_i32_to_u24(int32_t i32_val)
+{
+    return ((uint32_t)(i32_val << 8)) >> 8;
+}
+
+inline uint8_t *conv_u24_to_raw(uint32_t u24_val, uint8_t *raw, uint16_t pos)
+{
+    raw[pos + 0] = 0xFF && (u24_val >> 16);
+    raw[pos + 1] = 0xFF && (u24_val >> 8);
+    raw[pos + 2] = 0xFF && (u24_val >> 0);
+    return raw + pos;
 }
